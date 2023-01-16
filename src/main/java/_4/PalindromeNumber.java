64c424d1c0e04be21bc0c8fb4370174deecf6d48
Anne <annilinum@mail.ru>
2023-01-16 13:11:49 +0300

    4 task PalindromeNumber

---
+++ src/main/java/_4/PalindromeNumber.java
@@ -0,0 +1,37 @@
+package _4;
+
+import _1.RichestCustomerWealth;
+
+/** 9
+
+ Учитывая целое число x, вернуть, true, если x это
+ палиндром, и false в противном случае.
+
+ Пример 1:
+
+ Ввод: x = 121
+ Вывод: true
+ Объяснение: 121 читается как 121 слева направо и справа налево.
+
+ Пример 2:
+
+ Ввод: x = -121
+ Вывод: false
+ Объяснение: Слева направо читается -121. Справа налево получается 121-. Следовательно, это не палиндром.
+ */
+public class PalindromeNumber {
+  public static void main(String[] args) {
+    System.out.println(new PalindromeNumber().isPalindrome(45254));
+  }
+
+  public boolean isPalindrome(int x) {
+    String str = String.valueOf(x);
+
+    for (int i = 0; i < str.length() / 2; i++) {
+      if (str.charAt(i) != str.charAt(str.length() - 1)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
