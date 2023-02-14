15bb3b42c722f08c1790a85e97ed74cd425f735f
Anne <annilinum@mail.ru>
2023-02-14 13:08:14 +0400

    8 task ListNode

--- src/main/java/_8/ListNode.java
+++ src/main/java/_8/ListNode.java
@@ -1,18 +1,16 @@
 package _8;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 class Solution {
   public static void main(String[] args) {
-    ListNode list1 = makeList(1, 2, 4);
-    ListNode list2 = makeList(1, 3, 4);
+    ListNode list1 = makeList(5, 2, 4, 9);
+    ListNode list2 = makeList(1, 3, 6, 1);
 
     print(list1);
     print(list2);
 
-    print(new Solution().mergeTwoLists(list1, list2));
+    sumLists(list1, list2);
   }
 
   private static ListNode makeList(int... items) {
@@ -33,6 +31,35 @@ class Solution {
     System.out.println("]");
   }
 
+  public static int listToNumber(ListNode list) {
+    int n = 0;
+    while (list != null) {
+      n = 10 * n + list.val;
+      list = list.next;
+    }
+    return n;
+  }
+
+  public static void sumLists(ListNode list1, ListNode list2) {
+    int n = listToNumber(list1);
+    int m = listToNumber(list2);
+    System.out.println(n);
+    System.out.println(m);
+    int sum = n + m;
+    System.out.println(sum);
+    numberToArray(sum);
+  }
+
+  public static void numberToArray(int number) {
+    int length = String.valueOf(Math.abs(number)).length();
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++) {
+      result[i] = number % 10;
+      number = number / 10;
+    }
+    System.out.println(Arrays.toString(result));
+  }
+
   public static class ListNode {
     int val;
     ListNode next;
@@ -50,16 +77,25 @@ class Solution {
     }
   }
 
-  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+/*  public ListNode mergeTwoLists(ListNode list1, ListNode -) {
     if (list1 == null) return list2;
     if (list2 == null) return list1;
 
     if (list1.val < list2.val) {
       list1.next = mergeTwoLists(list1.next, list2);
       return list1;
-    } else {
-      list2.next = mergeTwoLists(list1, list2.next);
-      return list2;
     }
+
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }*/
+
+  public static void arrayToNumber(int[] array) {
+    int[] arr = {1, 2, 3, 4, 5};
+    int n = 0;
+    for (int i : arr) {
+      n = 10 * n + i;
+    }
+    System.out.println(n);
   }
 }
